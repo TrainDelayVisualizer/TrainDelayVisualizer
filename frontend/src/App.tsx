451674653ad7b8d6ccb6afce9548beda14c33019
patrick.wissiak <patrick.wissiak@ost.ch>
2024-03-11 09:44:48 +0100

    TDV-15 Fix warning in App.tsx and remove unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import logo from './logo.svg';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/*<Route path="/" element={<Home />} />*/}
+        <Route path="/" element={<Home />} />
         {/*<Route path="/about" element={<About />} />*/}
         {/*<Route path="/work" element={<Work />} />*/}
       </Routes>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hallo zäme.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
     </BrowserRouter>
   );
 }
