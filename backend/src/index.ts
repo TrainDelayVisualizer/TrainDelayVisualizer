9f69d3f8fd577e07e6abff5499900c7372413717
patrick.wissiak <patrick.wissiak@ost.ch>
2024-03-18 12:28:57 +0000

    TDV-16 wait for db on backend startup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,19 @@ import 'reflect-metadata';
 import { startServer } from './server';
 import Container from 'typedi';
 import { ApiImportService } from './services/import.service';
+import { CommandExecutorUtils } from './utils/command-executor.utils';
 
 console.log('************************');
 console.log(`* TrainDelayVisualizer *`);
 console.log('************************');
 
-startServer();
-
-// Auskommentieren, um den Import zu starten
- Container.get(ApiImportService).runFullImport();
+async function init() {
+    if (process.env.NODE_ENV === 'production') {
+        await CommandExecutorUtils.runCommand('cd /app/backend && npm run prisma-deploy');
+    }
+    // Auskommentieren, um den Import zu starten
+    Container.get(ApiImportService).runFullImport();
+    
+    startServer();
+}
+init();
