77ecb3923f6fd0a874af82d0ecb861d7cfe5ad45
biersoeckli <meow@biersoeckli.ch>
2024-03-22 11:07:26 +0000

    TDV-45 added job library pg-boss and JobSchedulerService

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import { startServer } from './server';
 import Container from 'typedi';
 import { ApiImportService } from './services/import.service';
 import { CommandExecutorUtils } from './utils/command-executor.utils';
+import { JobSchedulerService } from './services/job-scheduler.service';
+import { ApiImportJob } from './jobs/api-import.job';
 
 console.log('************************');
 console.log(`* TrainDelayVisualizer *`);
@@ -11,10 +13,10 @@ console.log('************************');
 async function init() {
     if (process.env.NODE_ENV === 'production') {
         await CommandExecutorUtils.runCommand('cd /app/backend && npm run prisma-deploy');
+        await Container.get(JobSchedulerService).configureAndScheduleJobs();
+        await Container.get(JobSchedulerService).runAllJobs();
+        console.log('Scheduled Jobs successfully.');
     }
-    // Auskommentieren, um den Import zu starten
-    Container.get(ApiImportService).runFullImport();
-    
     startServer();
 }
 init();
