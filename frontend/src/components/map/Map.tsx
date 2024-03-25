23f476f5c810420b4a33167c03c02e2274005cab
patrick.wissiak <patrick.wissiak@ost.ch>
2024-03-25 09:48:42 +0000

    TDV-43 Add MapController for zoom/pan change

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import { MapContainer, TileLayer } from "react-leaflet";
+import React, { useEffect, useRef, RefObject } from 'react';
+import { MapContainer, TileLayer, useMapEvents } from "react-leaflet";
 import "./Map.css";
 import "leaflet/dist/leaflet.css";
 
-function Map() {
+function MapController() {
+  const mapEvents = useMapEvents({
+    zoomend: () => {
+      console.log("zoom", mapEvents.getZoom());
+    },
+    moveend: () => {
+      console.log("pan", mapEvents.getCenter(), mapEvents.getBounds());
+    }
+  });
 
-  console.log("hio")
+  return null
+}
+
+function Map() {
+  const mapRef: RefObject<any> = useRef();
+
+  useEffect(() => {
+    function getLocation(): void {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const pos = [position.coords.latitude, position.coords.longitude];
+          mapRef.current.setView(pos, mapRef.current.getZoom());
+        });
+      }
+    }
+    getLocation();
+  }, []);
+
   return (
     <div className="App">
-       <MapContainer
-      className="map-container"
-      center={[47.2266, 8.81845]}
-      zoom={11}
-      maxZoom={18}
-    >
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-
-    </MapContainer>
+      <MapContainer
+        ref={mapRef}
+        className="map-container"
+        center={[47.2266, 8.81845]}
+        zoom={12}
+        maxZoom={13}
+        minZoom={10}
+      >
+        <MapController />
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+      </MapContainer>
     </div>
   )
 }
