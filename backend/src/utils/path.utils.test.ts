e1a309c7105d53d80e725b87d1fb5b8525d73b5e
biersoeckli <meow@biersoeckli.ch>
2024-04-15 13:27:48 +0000

    TDV-76 added test for boolean and path utils

--- backend/src/utils/path.utils.test.ts
+++ backend/src/utils/path.utils.test.ts
@@ -5,4 +5,16 @@ describe("PathUtils", () => {
   test("Test base path", () => {
     expect(PathUtils.getBasePath()).not.toBeNull();
   });
+
+  test("Test getTempPath", () => {
+    expect(PathUtils.getTempPath()).toMatch(/\/temp$/);
+  });
+
+  test("Test getDataPath", () => {
+    expect(PathUtils.getDataPath()).toMatch(/\/data$/);
+  });
+
+  test("Test getSbbImportDataPath", () => {
+    expect(PathUtils.getSbbImportDataPath()).toMatch(/\/sbb-import-data$/);
+  });
 });
