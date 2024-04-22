69392e48c7d7ae9e37f36dad42d73c0192a2d3d1
thomasruegg <hello@thomasruegg.com>
2024-04-22 06:53:24 +0000

    TDV-75 implement hotlines (enables gradients on lines on map)

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useRef, RefObject, useState } from 'react';
-import { MapContainer, TileLayer, useMapEvents, Popup, Marker, Polyline } from "react-leaflet";
+import { MapContainer, TileLayer, useMapEvents, Popup, Marker } from "react-leaflet";
 import { Progress } from 'antd';
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
 import { fetchStations, Station } from '../../store/stationSlice'
-import { fetchSections, Section } from '../../store/sectionSlice'
+import { fetchSections } from '../../store/sectionSlice'
+import { Hotline, HotlineOptions, Palette, HotlineProps } from 'react-leaflet-hotline';
 import "./Map.css";
 import "leaflet/dist/leaflet.css";
+import { JSX } from 'react';
 import L from 'leaflet';
+
 
 const icon = L.icon({
   iconUrl: "/ui/marker.svg",
@@ -29,6 +32,47 @@ function MapController() {
   return null
 }
 
+function HotlineWrapper<T>({ data, getLat, getLng, getVal, options, eventHandlers }: HotlineProps<T>): JSX.Element {
+  // Call the original Hotline component and return its result
+  return (
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    <Hotline
+      data={data}
+      getLat={getLat}
+      getLng={getLng}
+      getVal={getVal}
+      options={options}
+      eventHandlers={eventHandlers}
+    />
+  );
+}
+
+const data = [
+  { lat: 47.2266, lng: 8.81845, value: 1 },
+  { lat: 47.1, lng: 8.9, value: 2 },
+  { lat: 47.4, lng: 8.8, value: 3 },
+  { lat: 47.3, lng: 8.7, value: 4 },
+  { lat: 47.3, lng: 8.9, value: 4 },
+  { lat: 47.4, lng: 8.9, value: 4 },
+]
+
+export const palette_0: Palette = [
+  { r: 0, g: 160, b: 0, t: 0 },
+  { r: 255, g: 255, b: 0, t: 0.5 },
+  { r: 255, g: 0, b: 0, t: 1 }
+]
+
+const options: HotlineOptions = {
+  min: 1,
+  max: 8,
+  outlineWidth: 0,
+  outlineColor: 'black',
+  weight: 7,
+  palette: palette_0,
+  tolerance: 3
+}
+
 function Map() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const mapRef: RefObject<any> = useRef();
@@ -37,6 +81,8 @@ function Map() {
   const stations = useAppSelector((state) => state.station.all)
   const sections = useAppSelector((state) => state.section.all)
   const dispatch = useAppDispatch()
+
+  console.log("Sections: " + sections);
 
   useEffect(() => {
     function getLocation(): void {
@@ -70,18 +116,6 @@ function Map() {
     return () => clearTimeout(currTimeout);
   }, []);
 
-  function getColorByMinutesOfDelay(delay: number): string {
-    const DELAY_THRESHOLD_WARNING = 1;
-    const DELAY_THRESHOLD_CRITICAL = 2;
-
-    if (delay > DELAY_THRESHOLD_CRITICAL) {
-      return "red";
-    } else if (delay > DELAY_THRESHOLD_WARNING) {
-      return "orange";
-    }
-    return "green";
-  }
-
   return (
     <div className="App">
       <div className="loading-overlay" style={{ visibility: progress < 100 ? "visible" : "hidden", opacity: progress < 100 ? 1 : 0 }}>
@@ -102,22 +136,21 @@ function Map() {
             {station.lat.toFixed(4)}, {station.lon.toFixed(4)}
           </Popup>
         </Marker>)}
-        {sections.map((section: Section) => (
-          <Polyline
-            key={section.stationFrom.id + section.stationTo.id}
-            pathOptions={{ color: getColorByMinutesOfDelay(section.averageDepartureDelay) }}
-            positions={[
-              [section.stationFrom.lat, section.stationFrom.lon],
-              [section.stationTo.lat, section.stationTo.lon]
-            ]}
-          />
-        ))}
+
+        <HotlineWrapper
+          key={123}
+          data={data}
+          getLat={t => t.lat}
+          getLng={t => t.lng}
+          getVal={t => t.value}
+          options={{ ...options, tolerance: 10 }} />
+
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
       </MapContainer>
-    </div >
+    </div>
   )
 }
 
