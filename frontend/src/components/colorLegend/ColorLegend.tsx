9d34a3612d69c88b70aa3d158e0d515d967f5771
Thomas Rüegg <thomas.rueegg@ost.ch>
2024-05-13 13:32:49 +0200

    TDV-84 Add ColorLegend component for displaying delay legend

---
+++ frontend/src/components/colorLegend/ColorLegend.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Badge, Card, Space } from "antd";
+import "./ColorLegend.css";
+import { ColorLegendProps } from "../../model/props/ColorLegendProps";
+
+function ColorLegend({ isLineDelay }: ColorLegendProps) {
+    return (
+        <Card title="Delay Legend" className="color-legend">
+            <Space direction="vertical">
+                <Badge color="green" text="No delay" />
+                <Badge color="yellow" text={`≥ ${isLineDelay ? 3 : 1} min ${isLineDelay ? "" : "Ø"} delay`} />
+                <Badge color="red" text={`≥ ${isLineDelay ? 5 : 2} min ${isLineDelay ? "" : "Ø"} delay`} />
+                <Badge color="black" text="Cancelled" />
+            </Space>
+        </Card>
+    );
+}
+
+export default ColorLegend;
