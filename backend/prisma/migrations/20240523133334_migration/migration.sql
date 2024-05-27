7a8584dc8c8bae67f0690857933891f760c5f7f3
patrick.wissiak <patrick.wissiak@ost.ch>
2024-05-27 09:25:43 +0200

    add updated migration

--- backend/prisma/migrations/20240523133334_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `operator` to the `Line` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Line" ADD COLUMN     "operator" TEXT NOT NULL;
